00cc57e147b6492d79effe80b04b52dc15e9c229
Sunil Jadhav <45939197+gitsgulabjamun@users.noreply.github.com>
2025-09-01 03:07:40 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-// Quick Coach v3 – service worker
-const CACHE = "qc-cache-v3";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))) });
-self.addEventListener("fetch",(e)=>{const u=new URL(e.request.url); if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}});
